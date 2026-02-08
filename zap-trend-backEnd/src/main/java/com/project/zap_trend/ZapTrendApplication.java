458dd4eaf0f0509df4cbc9a34a181dc25b9b786e
sebastian-quiceno <sebastian.quicenol@upb.edu.co>
2026-02-08 09:28:45 -0500

    Agregacion de plantillas de backEnd y frontEnd

---
+++ zap-trend-backEnd/src/main/java/com/project/zap_trend/ZapTrendApplication.java
@@ -0,0 +1,13 @@
+package com.project.zap_trend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZapTrendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZapTrendApplication.class, args);
+	}
+
+}
